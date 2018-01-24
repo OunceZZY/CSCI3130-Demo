dfaec2d15434cf6140907d18e909e9f2d7c32272
zh946728@dal.ca <owen.zhang@dal.ca>
2018-01-24 03:42:23 -0400

    Finished

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    // The only added method (other than the interface)
+    public void copyMessage(View view) {
+        // search for the two windows, get the new string and replace with new string
+        EditText editText2 = (EditText) findViewById(R.id.editText2);
+        TextView textView = findViewById(R.id.textView);
+        String message = editText2.getText().toString();
+
+        textView.setText(message);
+
+
     }
 }
 
